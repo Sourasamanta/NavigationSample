a1404a67578a43722e2f8e33dfb2f21bc12ebee0
Sourajit Samanta <sourasamanta2004@gmail.com>
2025-08-22 23:34:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationsample/SecondScreen.kt
@@ -0,0 +1,43 @@
+package com.example.navigationsample
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigationsample.ui.theme.NavigationSampleTheme
+
+@Composable
+fun SecondScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "This is the First Screen")
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Welcome", fontSize = 24.sp)
+        Button(onClick = {
+
+        }) { Text(text = "Go to Second Screen") }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondScreenPreview() {
+    NavigationSampleTheme {
+        SecondScreen()
+    }
+}
